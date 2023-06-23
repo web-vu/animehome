ce6cb9bc5ae12aebd116044a36be18ff1f125cba
web-vu <pudge1top@gmail.com>
2023-06-23 07:54:23 +0300

    Add files

---
+++ src/components/AnimeView.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="anime-list">
+    <div class="anime-list-align">
+      <router-link class="anime-list-card-text" to="/anime/agent-of-time">
+        <div class="anime-list-card">
+          <img src="../assets/agent-of-time-anime.png" alt="" class="anime-list-card-img">
+          <p class="anime-list-card-text">Агент времени</p>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.anime-list {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.anime-list-align {
+  width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  gap: 50px;
+  flex-wrap: wrap;
+}
+.anime-list-card {
+  background: #ef8d8d;
+  width: 150px;
+  padding: 20px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+}
+.anime-list-card-img {
+  height: 200px;
+}
+.anime-list-card-text {
+  font-family: 'Segoe UI', sans-serif;
+  font-weight: 500;
+  text-decoration: none;
+  color: white;
+}
+</style>
---
+++ src/components/anime/AgentOfTime.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Смотреть аниме Агент времени</h1>
+  <div class="anime-content">
+    <iframe src="//kodik.info/serial/34780/24abbcd1fbb19105f15f8055a8a64c37/720p" width="607" height="360" frameborder="0" AllowFullScreen allow="autoplay *; fullscreen *"></iframe>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    font-family: 'Segoe UI', sans-serif;
+    font-weight: 400;
+    font-size: 24px;
+    text-align: center;
+    margin-top: 25px;
+  }
+  .anime-content {
+    display: flex;
+    justify-content: center;
+    margin-top: 25px;
+  }
+</style>
